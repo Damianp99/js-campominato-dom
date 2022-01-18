cbe0228f20b2927d9614a0eab475384713baf2b3
Damianp99 <damianperez200199@gmail.com>
2022-01-18 14:45:09 +0100

    Starting

---
+++ js/minato.js
@@ -0,0 +1,15 @@
+/*
+
+1-Prendo il mio button 
+2-Aggancio un even listener (funzione play())
+    -Cambio il testo del button da 'gioca' => 'ricomincia'
+    -Svuoto l'eventuale contenuto di #grid
+
+3-Settare che le bombe sono 16
+4-Raccogliamo il livello dalla select
+5-Calcolo in base al livello scelto , quante sono le celle 'safe'
+6-Genero i numeri delle bombe 
+7-Renderizziamo la griglia
+8-Aggiungiamo 'event listener' alle celle
+
+*/
